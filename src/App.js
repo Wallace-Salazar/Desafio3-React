ec4557d4a32212b0dbc6dfff0a7eb7f8b20e3bfe
Wallace Oliveira <104787895+Wallace-Salazar@users.noreply.github.com>
2022-07-13 00:12:09 -0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+//Function Component
+
+import React from "react";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div>
+      <h1>
+        “Qualquer um pode escrever um código que o computador entenda. Bons
+        programadores escrevem códigos que os humanos entendam.”
+      </h1>
+      <p>– Martin Fowler</p>
+    </div>
+  );
+}
